c48987fe814e1272abade3e967aca39d6214c76f
aaron2024 <xieaaron@hotmail.com>
2020-12-22 02:46:30 -0800

    minor update

--- src/components/pageTwo/ChangeDayModal.js
+++ src/components/pageTwo/ChangeDayModal.js
@@ -16,6 +16,11 @@ function ChangeDayModal({ isOpen, close, updateDay }) {
         close();
     }
 
+    function pendingDay(){
+        alert('Sorry recipe is in development, check back later');
+        close();
+    }
+
     return ReactDom.createPortal(
         <>
             <div className={'changeDayPop__overlay'}></div>
@@ -28,6 +33,17 @@ function ChangeDayModal({ isOpen, close, updateDay }) {
                 </div>
                 <div onClick={()=>choosenDay('Day Two')} className={'changeDayPop__dayTwo'}>
                     <h3>Day Two</h3>
+                </div>
+
+
+                <div onClick={()=>pendingDay()} className={'changeDayPop__dayThree'}>
+                    <h3>Day Three</h3>
+                </div>
+                <div onClick={()=>pendingDay()} className={'changeDayPop__dayFour'}>
+                    <h3>Day Four</h3>
+                </div>
+                <div onClick={()=>pendingDay()} className={'changeDayPop__dayFive'}>
+                    <h3>Day Five</h3>
                 </div>
 
                 <p className={'changeDayPop__note'}>...im working on more. Let me know if you wanna lend a hand!</p>
